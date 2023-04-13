07076136c98c33b156c1b617a746fb3f9e462bc4
Pers test <qa61test@gmail.com>
2023-04-13 22:03:55 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int costTiket = 18666;
+        int bonus;
+
+        if (costTiket > 20) {
+            bonus = costTiket / 20;
+        }
+        else {
+            bonus=0;
+        }
+        System.out.println("Стоимость билета = " + costTiket);
+        System.out.println("Вам начислится " + bonus + " бонуса(ов)");
+
+    }
+} 
